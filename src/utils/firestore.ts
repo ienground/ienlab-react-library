18e7d55334d474029219dc7be7c605c7b8d6964b
IENGROUND <ericano.rhee@gmail.com>
2026-04-21 08:40:25 +0900

    ✨ feat(locales): 다국어 지원을 위한 초기 i18next 구성 추가
    영문 및 한국어 JSON 번역 파일 추가.
    peerDependencies에 i18next 및 react-i18next 패키지 추가.
    다국어 설정 초기화를 위한 index.ts 파일 구성.

--- src/utils/firestore.ts
+++ src/utils/firestore.ts
@@ -1,8 +1,62 @@
-import type {DocumentData, DocumentSnapshot, QueryDocumentSnapshot } from "firebase/firestore";
+import {collection, documentId, getDoc, getDocs, query,
+  where, type DocumentData, type DocumentReference, type DocumentSnapshot, type QueryDocumentSnapshot, type Firestore} from "firebase/firestore";
+import type {FirestoreItem} from "../types";
 
 export function snapshotToData(snapshot: QueryDocumentSnapshot | DocumentSnapshot): DocumentData {
   return {
     ...(snapshot.data() as DocumentData),
     id: snapshot.id,
   };
+}
+
+export async function fetchItems<T extends FirestoreItem>(
+  firestore: Firestore,
+  colName: string,
+  changeMethod: ((snapshot: QueryDocumentSnapshot | DocumentSnapshot) => T),
+  cache: Map<string, T>,
+  referenceArray: (DocumentReference | undefined)[] // 💡 DocumentReference 배열
+) {
+  const cached: T[] = referenceArray
+    .map(ref => cache.get(ref?.path ?? ""))
+    .filter(Boolean) as T[];
+
+  const cachedPaths = new Set(cached.map(item => item.id));
+  const missingRefs = referenceArray.filter(ref => (ref && !cachedPaths.has(ref.path)));
+
+  const chunkSize = 30;
+  for (let i = 0; i < missingRefs.length; i += chunkSize) {
+    const chunk = missingRefs.slice(i, i + chunkSize);
+
+    // 💡 DocumentReference 청크를 'in' 쿼리에 직접 사용
+    const q = query(
+      collection(firestore, colName),
+      where(documentId(), 'in', chunk) // 쿼리할 문서의 필드 이름으로 변경
+    );
+
+    const docs = await getDocs(q);
+
+    docs.forEach(snapshot => {
+      const item = changeMethod(snapshot as QueryDocumentSnapshot);
+      cache.set(item.id, item);
+    });
+  }
+}
+
+export async function fetchItemsByOne<T extends FirestoreItem>(
+  changeMethod: ((snapshot: QueryDocumentSnapshot | DocumentSnapshot) => T),
+  cache: Map<string, T>,
+  referenceArray: (DocumentReference | undefined)[] // 💡 DocumentReference 배열
+) {
+  const cached: T[] = referenceArray
+    .map(ref => cache.get(ref?.path ?? ""))
+    .filter(Boolean) as T[];
+
+  const cachedPaths = new Set(cached.map(item => item.id));
+  const missingRefs = referenceArray.filter(ref => (ref && !cachedPaths.has(ref.path)));
+
+  for (const ref of missingRefs) {
+    if (!ref) continue;
+    const item = await getDoc(ref).then(changeMethod);
+    cache.set(item.id, item);
+  }
 }
