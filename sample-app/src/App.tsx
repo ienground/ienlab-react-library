f87b039545132b445528f071494a611868649641
IENGROUND <my@ien.zone>
2026-05-22 01:36:19 +0900

    ♻️ refactor(core): App.tsx 컴포넌트 리팩토링
    - ImageUploadSortableField 컴포넌트 속성 정의 및 구조 개선
    - 썸네일 업로드 관련 props 추가 및 초기화 로직 수정
    - 컴포넌트 내부 구조 및 레이아웃 변경사항 반영

--- sample-app/src/App.tsx
+++ sample-app/src/App.tsx
@@ -71,7 +71,6 @@ export default function App() {
             //   FieldDescription,
             // }}
           />
-          dddddd
           <ImageUploadSortableField
             id="thumbnail"
             label="썸네일"
