53000dc3c2ff45a49aead5fe4425f132884f4010
IENGROUND <my@ien.zone>
2026-05-22 11:09:50 +0900

    ✨ feat(core): Firebase 이미지 업로드 유틸리티 추가
    - Firebase Storage를 활용한 이미지 업로드 및 다운로드 URL 추출 함수 구현
    - 파일 확장자 추출 로직 추가 및 유틸리티 모듈 내보내기 설정
    - 기존 파일에 새로운 유틸리티 함수 추가 및 export 구문 업데이트

---
+++ src/utils/fb-storage.ts
@@ -0,0 +1,15 @@
+import {getDownloadURL as getFbDownloadURL, ref, uploadBytes, type FirebaseStorage } from "firebase/storage";
+import {ImageUploadItem} from "../types";
+import {getFileExtension} from "./file.ts";
+
+/**
+ * @param storage Firebase Storage 객체
+ * @param path 확장자를 제외한 파일 경로 및 이름
+ * @param item 업로드할 이미지
+ * @return 업로드된 이미지의 URL
+ */
+export async function getDownloadURL(storage: FirebaseStorage, path: string, item: ImageUploadItem) {
+  if (!item.file) return item.url
+  const imageRef = ref(storage, `${path}.${getFileExtension(item.file.name)}`)
+  return await uploadBytes(imageRef, item.file).then(result => getFbDownloadURL(result.ref))
+}
